39e97efc3731bb51390c02ca6bf0328e462f320a
Joy Lu <codejoy9d2@gmail.com>
2022-12-27 19:10:03 -0800

    refactoring code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,71 @@
 
-import './App.css'
-import SearchBar from './components/SearchBar'
+import { useState } from 'react';
+import Cards from './components/Cards';
+import axios from 'axios';
 
 const App = () => {
+  const [searchTitle, setSearchTitle] = useState('');
+  const [bookAuthor, setBookAuthor] = useState('');
+  const [books, setBooks] = useState([]);//fetches books with author and title
+
+  const getBooks = async (title) => {
+    const baseURL = 'https://www.googleapis.com/books/v1/volumes?q=';
+    const query = title.split(' ').join('+');
+    const author = bookAuthor.split(' ').join('+');
+    const index = 0;
+    const url = `${baseURL}${query}:${author}`;
+
+    try {
+      const response = await axios.get(url);
+      setBooks(response.data.items)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleSearchTitle = (e) => {//updates book title state
+    let search = e.target.value;
+    setSearchTitle(search.toLowerCase());
+  }
+
+  const handleBookAuthor = (e) => {//updates book author
+    let author = e.target.value;
+    setBookAuthor(author.toLowerCase());
+  }
+  
+  const handleSearch = () => { 
+    let title = searchTitle.includes(' ') ? [searchTitle.split(' ')].join('+') : searchTitle;
+
+    getBooks(title);
+  }
+  console.log(books[0], 'book');
   return (
-    <div className="container">
-      <h1 className="title">Book Finder</h1>
-      <SearchBar />
-      <Cards />
+      
+    <div>
+      <header className='header'>
+        <div className="header__content container">
+          <h1 className="title">Book Finder</h1>
+          <div className="search-bar">
+            <div className="search__inputs">
+              <input className="book-title" type="text" id="searchbar" name="searchbar" placeholder="Search book title"
+                onChange={(e) => setSearchTitle(e.target.value)}
+              />
+              <input className="book-author" type="text" id="searchbar" name="searchbar" placeholder="Book author"
+                onChange={handleBookAuthor}
+              />
+            </div>
+              
+            <button className="search-btn" type="submit" onClick={() => getBooks(searchTitle)}>Search</button>
+          </div>
+        </div>
+      </header>
+
+      <Cards books={books} />
     </div>
+
   )
 }
 
 export default App;
+//api key:
+//AIzaSyC44xtP0-GcP13n6dJaNQzrcTKdBoYaGWY
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,19 +1,23 @@
-import { useEffect } from 'react';
 
-//takes in the title state from search
-//searches the data for the titles
-//should return an obj or arr
+
 //use map on data and pass each into card component
-const Cards = (bookSearchTitle) => {
-
-
-
-
-
-
+const Cards = ({ books }) => {
+  
   return (
-    <div className="cards">
-      
+    <div className="cards container">
+      {books.map((book, index) => {
+        return (
+          <div className='card' key={index}>
+            <h2 className="card__title">{book.volumeInfo?.title}</h2>
+            <img className="card__img" src={book.volumeInfo.imageLinks?.thumbnail} alt="book cover" />
+            <div className="card__info">
+              <p className="card__author"><span>Author(s):</span> {[book.volumeInfo.authors].join(',')}</p>
+              <p className="card__publisher"><span>Published By:</span> {book.volumeInfo.publisher}</p>
+            </div>
+              <button className="card__btn-cta"><a href={book.volumeInfo?.infoLink}>See This Book</a></button>
+          </div>
+        )
+       })}
     </div>
   )
 
